44bd2f304f9469041eb422d1b1a7b5efc25d4f05
rulerhao <z19970615@gmail.com>
2022-12-26 09:31:18 +0800

    1. Shorter the code of StatysTypes to make it more readable.

---
+++ app/src/main/java/com/rulhouse/openexchangeratesdemo/remote/rates/error/StatusTypes.kt
@@ -0,0 +1,33 @@
+package com.rulhouse.openexchangeratesdemo.remote.rates.error
+
+enum class StatusTypes {
+    NotFound,
+    MissingAppId,
+    InvalidAppId,
+    NotAllowed,
+    AccessRestricted,
+    InvalidBase,
+    Success,
+    NetworkError,
+    UnknownError;
+
+    companion object {
+        fun mapToStatus(code: Int, message: String?): StatusTypes {
+            when (code) {
+                429 -> return NotAllowed
+                404 -> return NotFound
+                403 -> return AccessRestricted
+                401 -> {
+                    when (message) {
+                        "invalid_app_id" -> return InvalidAppId
+                        "missing_app_id" -> return MissingAppId
+                    }
+                }
+                400 -> return InvalidBase
+                200 -> return Success
+            }
+
+            return UnknownError
+        }
+    }
+}
