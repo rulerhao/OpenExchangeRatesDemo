a7418b13979f6ce90bfa74d00fcf967b1e2692bf
rulerhao <z19970615@gmail.com>
2022-12-28 06:25:51 +0800

    1. Wrapped events to DropdownMenuEvent.

--- app/src/main/java/com/rulhouse/openexchangeratesdemo/view/components/drop_down_menu/DropdownMenu.kt
+++ app/src/main/java/com/rulhouse/openexchangeratesdemo/view/components/drop_down_menu/DropdownMenu.kt
@@ -8,32 +8,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.rulhouse.openexchangeratesdemo.remote.rates.dto.CurrencyRate
+import com.rulhouse.openexchangeratesdemo.view.components.drop_down_menu.DropdownMenuEvent
 
 @Composable
 fun DropDownMenu(
     item: CurrencyRate,
     expanded: Boolean,
-    onExpand: () -> Unit,
-    onDismissRequest: () -> Unit,
-    onItemClick: (CurrencyRate) -> Unit,
+    onEvent: (DropdownMenuEvent) -> Unit,
     list: List<CurrencyRate>
 ) {
     Button(
-        onClick = {
-            onExpand()
-        },
+        onClick = { onEvent(DropdownMenuEvent.OnExpand) },
         Modifier.wrapContentSize()
     ) {
         Text(text = item.currency)
     }
     DropdownMenu(
         expanded = expanded,
-        onDismissRequest = { onDismissRequest() }
+        onDismissRequest = { onEvent(DropdownMenuEvent.OnDismissRequest) },
     ) {
         list.forEach {
             DropdownMenuItem(
                 text = { Text(text = it.value.toString()) },
-                onClick = { onItemClick(it) },
+                onClick = { onEvent(DropdownMenuEvent.OnRateSelect(it)) },
                 leadingIcon = { Text(text = it.currency) }
             )
         }
