4deb611dad37813bee49152fc2634d79fdcb2da0
rulerhao <z19970615@gmail.com>
2022-12-28 06:43:33 +0800

    1. Convert Rate type to CurrencyRate interface to improve the extendability.

--- app/src/main/java/com/rulhouse/openexchangeratesdemo/view/components/currency_button/CurrencyButton.kt
+++ app/src/main/java/com/rulhouse/openexchangeratesdemo/view/components/currency_button/CurrencyButton.kt
@@ -3,11 +3,11 @@ package com.rulhouse.openexchangeratesdemo.view.components.currency_button
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.rulhouse.openexchangeratesdemo.remote.rates.dto.Rate
+import com.rulhouse.openexchangeratesdemo.remote.rates.dto.CurrencyRate
 
 @Composable
 fun CurrencyButton(
-    rate: Rate,
+    rate: CurrencyRate,
     onButtonEvent: (CurrencyButtonEvent) -> Unit
 ) {
     Button(
