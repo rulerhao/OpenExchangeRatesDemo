5ae8b5a9b61a0bd011eff1e23c28f30a54c7ac4d
rulerhao <z19970615@gmail.com>
2022-12-28 15:43:26 +0800

    1. Added the MainScreen to MainActivity.

--- app/src/main/java/com/rulhouse/openexchangeratesdemo/MainActivity.kt
+++ app/src/main/java/com/rulhouse/openexchangeratesdemo/MainActivity.kt
@@ -6,38 +6,26 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.rulhouse.openexchangeratesdemo.ui.theme.OpenExchangeRatesDemoTheme
+import com.rulhouse.openexchangeratesdemo.view.MainScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             OpenExchangeRatesDemoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    OpenExchangeRatesDemoTheme {
-        Greeting("Android")
     }
 }
